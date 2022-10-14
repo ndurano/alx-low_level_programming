6c7ba779c2245213fdcc8b6557beb0e259e489a6
Ndurano <normanndurano@gmail.com>
2022-10-14 15:46:09 -0700

    Use Printf to output a string value

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: 'The program uses puts to output a string value
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char MyString[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s",MyString);
+
+	return (0);
+
+}
