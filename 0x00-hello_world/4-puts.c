0d5a1a28ae378630300f2b2e2317210aed77a4f0
Ndurano <normanndurano@gmail.com>
2022-10-14 15:08:38 -0700

    Use Puts to output a string value

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: 'The program uses puts to output a string value
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char MyString[] = "Programming is like building a multilingual puzzle";
+
+	puts(MyString);
+
+	return (0);
+
+}
