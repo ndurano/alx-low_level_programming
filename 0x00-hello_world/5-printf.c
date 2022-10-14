6b1dbdbfbd7616e00d91beaa8327bd29b492a96a
Ndurano <normanndurano@gmail.com>
2022-10-14 16:06:02 -0700

    Use Printf to output a string value

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -11,7 +11,7 @@
 int main(void)
 
 {	
-	printf("%s","with proper grammar, but the outcome is a piece of art,");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,");
 
 	return (0);
 
