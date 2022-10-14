be7c035c1f3692ce82cb2f9f5e22605c820b883f
Ndurano <normanndurano@gmail.com>
2022-10-14 15:18:28 -0700

    Use Puts to output a string value

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,7 @@
 int main(void)
 
 {
-	char MyString[] = "Programming is like building a multilingual puzzle";
+	char MyString[] ="\"Programming is like building a multilingual puzzle";
 
 	puts(MyString);
 
