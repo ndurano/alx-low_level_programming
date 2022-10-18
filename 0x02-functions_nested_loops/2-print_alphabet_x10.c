993ea6276d7d59a66cee51f39244a0b06e7262e8
Ndurano <normanndurano@gmail.com>
2022-10-18 13:48:04 -0700

    Functions and Nested Loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,35 @@
+#include<main.h>
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: Program that prints the alphabet 10 times
+ *
+ * Return: Nothing
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+
+	int b = 0;
+
+	while (b < 10)
+
+	{
+
+		a = 'a';
+
+		while (a <= 'z')
+
+		{
+			_putchar(a);
+
+			a += a;
+		}
+
+		_putchar(10);
+
+		b += a;
+
+	}
+
+}
