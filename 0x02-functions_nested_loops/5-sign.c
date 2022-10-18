c2d9bac0531839473475dd3bca6fac0246af1614
Ndurano <normanndurano@gmail.com>
2022-10-18 14:59:28 -0700

    Functions and Nested Loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ *
+ * Description: Function that checks the sign of a number
+ *
+ *@n: The character to check
+ * Return: int
+ */
+int print_sign(int n)
+
+{
+	if (n > 0)
+
+	{
+
+		_putchar(43);
+
+		return (1);
+
+	}
+
+	else if (n < 0)
+
+	{
+
+		_putchar(45);
+
+		return (-1);
+
+	}
+
+	else
+
+	{
+
+		_putchar(48);
+
+		return (0);
+
+	}
+
+}
