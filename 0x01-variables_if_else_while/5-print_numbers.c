40870f8238b3d5c3a3c56f37560955216da65c97
Ndurano <normanndurano@gmail.com>
2022-10-19 13:06:47 -0700

    Variables and conditional statement

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digit numbers of base
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+
+	int number = 0;
+
+
+
+	while (number <= 9)
+
+	{
+
+		printf("%i", number);
+
+		++number;
+
+	}
+
+	printf("\n");
+
+
+
+	return (0);
+
+}
