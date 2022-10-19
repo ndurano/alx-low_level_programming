37f8dd1082312365ea5fbab5eaaff10745b9d872
Ndurano <normanndurano@gmail.com>
2022-10-19 13:14:53 -0700

    Variables and conditional statement

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print alphabets in reverse
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+
+	char alphabet = 'z';
+
+
+
+	while (alphabet >= 'a')
+
+	{
+
+		putchar(alphabet);
+
+		--alphabet;
+
+	}
+
+	putchar('\n');
+
+
+
+	return (0);
+
+}
