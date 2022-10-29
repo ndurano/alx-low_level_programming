a451bb6faf6c65520577ade234e489265e4f9cc1
Ndurano <normanndurano@gmail.com>
2022-10-29 15:52:30 -0700

    pointers, arrays and strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,74 +1,25 @@
 #include "main.h"
 /**
- * digits - Numbers digits of a number
- * @n: Number
- * Return: Digits
- */
-unsigned int digits(unsigned int n)
-{
-	int i;
-
-	i = 0;
-	while (n != 0)
-	{
-		i++;
-		n /= 10;
-	}
-
-	return (i);
-}
-
-/**
- * pow_x10 - Custom pow
- * @exponent: Exponent number
- * Return: Return the divisor
- */
-
-unsigned int pow_x10(unsigned int exponent)
-{
-	unsigned int res, i;
-
-	res = 1;
-	for (i = 1; i < exponent; i++)
-	{
-		res *= 10;
-	}
-
-	return (res);
-}
-/**
- * print_number - Print a number
- * @n: Number
+ *print_number - prints an integer.
+ *
+ *@n: integer to be printed.
+ *
+ *Return: nothing
  */
 void print_number(int n)
 {
-	unsigned int div, aux, dig;
+	unsigned int num;
 
-	if (n == 0)
+	num = n;
+	if (n < 0)
 	{
-		_putchar('0');
-		return;
-	}
-	else if (n < 0)
-	{
-		aux = -n;
-		_putchar('-');
-	} else
-	{
-		aux = n;
+		_putchar(45);
+		num = -n;
 	}
 
-	dig = digits(aux);
-	div = pow_x10(dig);
-	while (aux > 0)
+	if (num / 10)
 	{
-		_putchar(aux / div + 48);
-		aux = aux % div;
-		div = div / 10;
-		dig--;
-
-		if (aux == 0 && dig == 1)
-			_putchar('0');
+		print_number(num / 10);
 	}
-
+	_putchar((num % 10) + '0');
 }
