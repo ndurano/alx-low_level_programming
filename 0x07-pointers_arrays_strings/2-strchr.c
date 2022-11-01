a5763451839bab9f8308fb8c6c843db9cd9b43b9
Ndurano <normanndurano@gmail.com>
2022-11-01 15:44:48 -0700

    pointers, arrays and functions

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,15 +9,19 @@
 
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if (*s == c)
-            return (s);
-        s++;
-    }
-    
-    if (*s == c)
-        return (s);
-    /*return null if c is not found*/
-    return ('\0');
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	/**
+	 * if c is '\0', you should return
+	 * the pointer to the '\0' of the
+	 * string s
+	*/
+	if (*s == c)
+		return (s);
+	/*return null if not found*/
+	return ('\0');
 }
